2bbec70da958d4020c85499fa0266959f69acf4a
Cataline Baert <snuf_kin@yahoo.com>
2019-08-06 14:29:16 +0200

    implement new and create methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,10 +8,19 @@ class TasksController < ApplicationController
   end
 
   def new
-
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to tasks_path
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
