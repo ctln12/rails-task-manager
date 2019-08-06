3b32b90d8288aefe17d66c500295ffdfe1eadf49
Cataline Baert <snuf_kin@yahoo.com>
2019-08-06 15:21:20 +0200

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   get '/tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
   patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks', to: 'tasks#destroy'
 end
